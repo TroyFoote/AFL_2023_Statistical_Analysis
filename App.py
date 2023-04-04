97c976c441c75cc4181260d420ea40c0062f9daf
TroyFoote <troyfoote@gmail.com>
2023-04-04 21:22:25 +0800

    completed flask app and created css file

--- App.py
+++ App.py
@@ -1,34 +1,54 @@
 # import Flask
 from flask import Flask, jsonify
 from flask_pymongo import PyMongo
-
+from pymongo import MongoClient
+from flask_cors import CORS
 
 # Create an app, being sure to pass __name__
 app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/AFL_Season_Analysis"
 mongo = PyMongo(app)
 
+# Create an instance of MongoClient
+mongo = MongoClient(port=27017)
+
+# Declare the database
+db = mongo.AFL_Season_Analysis_db
+
+# Declare the collections
+AFL_Season_Disposals = db.Disposals
+AFL_Season_Goals = db.Goals
 
 # Flask routes
 @app.route("/")
 def home():
     """List all available API routes."""
-    print("Server received request for welcome page")
+    print("Server received request for home page")
     return (
         f"Available Routes<br/>"
-        f"/api/v1.0/disposals<br/>"
-        f"/api/v1.0/goals<br/>"
+        f"/api/v1.0/Disposals<br/>"
+        f"/api/v1.0/Goals<br/>"
     )
 
-@app.route("/api/v1.0/disposals")
-def precipitation():
-    print("Server received request for disposals page")
+@app.route("/api/v1.0/Disposals")
+def Disposals():
+    Query = {}
+    Fields = {'_id':0,'Season':1,'Total_Disposals':1,'Teams':1,'Ave_disposals':1,'Most Disposals':1,'%_of_disposals':1}
+    Results = list(AFL_Season_Disposals.find(Query, Fields))
+    print(Results)
+    return jsonify(Results)
 
 
-season_disposals = session.query(Measurement.date, Measurement.prcp).all()    
+@app.route("/api/v1.0/Goals")
+def Goals():
+    Query = {}
+    Fields = {'_id':0,'Season':1,'Games_Played':1,'Goals':1,'Ave_goals_per_season':1,'Most Goals':1,'GK_Team':1,'Goals_Kicked':1,'Percentage_of_goals':1}
+    Results = list(AFL_Season_Goals.find(Query, Fields))
+    print(Results)
+    return jsonify(Results)     
 
-@app.route("/api/v1.0/goals")
-def precipitation():
-    print("Server received request for goals page")    
-          
 
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- scoring_summary_plots.js
+++ scoring_summary_plots.js
@@ -1,46 +1,18 @@
-// Initialised arrays
-let Season = [];
-let Ave_goals = [];
-let Goals_Kicked = [];
-let Percentage_of_goals = [];
 
-// For loop to populate arrays
-for (let i = 0; i < searchResults.length; i++) {
-  row = searchResults[i];
-  names.push(row.pair);
-  greekNames.push(row.greekName);
-  romanNames.push(row.romanName);
-  greekSearchResults.push(row.greekSearchResults);
-  romanSearchResults.push(row.romanSearchResults);
-}
+// const URL = http://127.0.0.1:5000/api/v1.0/Disposals
 
-// Trace1 for the Greek Data
+
 let trace1 = {
-  x: Season,
-  y: Percentage_of_goals,
-  text: greekNames,
-  name: "Greek",
-  type: "bar"
+  x: xData,
+  y: yData
 };
 
-// Create data array
 let data = [trace1];
 
-// Apply a title to the layout
 let layout = {
-  title: "search results",
-  barmode: "group",
-  // Include margins in the layout so the x-tick labels display correctly
-  margin: {
-    l: 50,
-    r: 50,
-    b: 200,
-    t: 50,
-    pad: 4
-  }
+  title: "A Plotly plot"
 };
 
-// Render the plot to the div tag with id "plot"
 Plotly.newPlot("plot", data, layout);
 
 
